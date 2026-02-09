07a37b4b7ca8c99a93fbd35eeb85b4226895205d
Sreeram <sreeram@github.com>
2026-02-09 23:22:49 +0530

    changed cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,6 +13,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/image_kyc_db';
 
+const allowedOrigins=[
+    "http://localhost:5173",
+    ""
+]
+
 // Middleware
 app.use(cors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
